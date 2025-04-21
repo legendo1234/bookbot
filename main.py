e4e7161595d6657e6bcfed7764521df44ea4fd55
legendo1234 <matej.weber@gmail.com>
2025-04-21 19:09:41 +0200

    made a new file and imported a function from there to main

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from stats import number_of_words
+
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(text)    
+    words = number_of_words(text)   
+    print(f"{words} words found in the document") 
+
+    all_words = text.split()
+    print("First 10 words:", all_words[:10])
+    print("Last 10 words:", all_words[-10:])
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-    
 
 if __name__ == "__main__":
     main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def number_of_words(text):
+    # Try splitting by whitespace to get all words
+    words = text.split()
+    return len(words)
